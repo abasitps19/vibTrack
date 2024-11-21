ec3a61f541869b195281c60fb179353fb595dc80
abasitps19 <abasitps19@gmail.com>
2024-11-21 23:05:12 +0500

    -configuration build added

---
+++ build/image_preload.cmake
@@ -0,0 +1,7 @@
+# Generated file that can be used to preload variant images
+set(BOARD "ebyte_e73_tbb/nrf52832" CACHE INTERNAL "NCS child image controlled")
+set(BOARD_ROOT "e:/project/ble_boards;e:/project/innovation/bluetoothmotiondt/track/vibtrack;c:/my_boards" CACHE INTERNAL "NCS child image controlled")
+set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
+set(DTC_OVERLAY_FILE "" CACHE INTERNAL "NCS child image controlled")
+set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
+set(WEST_PYTHON "C:/ncs/toolchains/ce3b5ff664/opt/bin/python.exe" CACHE INTERNAL "NCS child image controlled")
---
+++ build/zephyr/include/generated/app_commit.h
@@ -0,0 +1,12 @@
+#ifndef _APP_COMMIT_H_
+#define _APP_COMMIT_H_
+
+/*  values come from cmake/version.cmake
+ * BUILD_COMMIT related  values will be 'git rev-parse',
+ * alternatively user defined BUILD_VERSION.
+ */
+
+#define APP_COMMIT                   b62882decd2e
+#define APP_COMMIT_STRING            "b62882decd2e"
+
+#endif /* _APP_COMMIT_H_ */
